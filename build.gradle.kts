3d514f5c8590b242a44f459507e2736b24c31292
samuele794 <samuelebruschi794@gmail.com>
2025-03-21 16:25:49 +0100

    Add statusPage error

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging")
     implementation("io.ktor:ktor-server-resources")
     implementation("io.ktor:ktor-server-sessions")
+    implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-swagger")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,8 +1,11 @@
 package com.takaotech
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.event.Level
 
@@ -10,6 +13,16 @@ fun Application.configureHTTP() {
     install(CallLogging) {
         level = Level.DEBUG
     }
+
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, _ ->
+            call.respondText(
+                text = "404: Errore",
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
+
     routing {
         swaggerUI(path = "openapi")
     }
